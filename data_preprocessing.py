8c646638501bb80794b9dd205cd012b0fe369cd1
ascheppach <56842416+ascheppach@users.noreply.github.com>
2021-01-19 14:39:08 +0100

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 25 19:47:29 2020
+
+@author: amadeu
+"""
+######## data preprocessing #####
+import torch
+from collections import Counter
+import numpy as np
+from numpy import array
+from torch.utils.data import Dataset,DataLoader
+
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+        
+def data_preprocessing(data_file, seq_size, representation, model_type, batch_size):
+    
+    def open_data(data_file):
+        with open(data_file, 'r') as f:
+            text = f.read()
+        
+        def split(text): 
+            return [char for char in text] 
+            
+        text = split(text)
+        
+        text = text[:-1]#because last row/observation is empty space
+        #text = text[0:100000]
+            
+        word_counts = Counter(text)# counter object is needed for sorted_vocab bzw. int_to_vacab and vocab_to_int 
+        sorted_vocab = sorted(word_counts, key=word_counts.get, reverse=True)
+        int_to_vocab = {k: w for k, w in enumerate(sorted_vocab)}
+        vocab_to_int = {w: k for k, w in int_to_vocab.items()}
+        num_classes = len(int_to_vocab)
+        
+        int_text = [vocab_to_int[w] for w in text] 
+        
+        text = np.asarray(int_text)
+        #text = text[0:20000]
+        
+        return text, num_classes, int_to_vocab, vocab_to_int
+
+
+    def create_sequences(in_text, seq_size, representation, model_type): 
+        x = list()
+        y = list()
+        
+        if (representation == 'onehot'):
+                int_enc = in_text.reshape(len(in_text), 1)
+                in_text = OneHotEncoder(sparse=False).fit_transform(int_enc)
+        
+                text_len = len(in_text)
+                for i in range(text_len):
+                    
+                    idx = i + seq_size #sequence end
+                    
+                    if idx > len(in_text)-1: 
+                        break
+                    if (model_type == 'CNN'): # because CNN, needs one prediction for each sequence (target)
+                        feat_seq, target_seq = in_text[i:idx], in_text[idx] # target labels for CNN
+                        x.append(feat_seq)
+                        y.append(target_seq)
+                    if (model_type == 'LSTM'): # because LSTM, needs for each step of the sequence a prediction (target)
+                        feat_seq, target_seq = in_text[i:idx], in_text[(i+1):(idx+1)] # target labels for LSTM
+                        x.append(feat_seq)
+                        y.append(target_seq)
+            
+        if (representation == 'embedding'):
+        
+                text_len = len(in_text)
+                for i in range(text_len):
+                    
+                    idx = i + seq_size #sequence end
+                    
+                    if idx > len(in_text)-1: 
+                        break
+                    if (model_type == 'CNN'): # because CNN, needs one prediction for each sequence (target)
+                        feat_seq, target_seq = in_text[i:idx], in_text[idx] # target labels for CNN
+                        x.append(feat_seq)
+                        y.append(target_seq)
+                    if (model_type == 'LSTM'): # because LSTM, needs for each step of the sequence a prediction (target)
+                        feat_seq, target_seq = in_text[i:idx], in_text[(i+1):(idx+1)] # target labels for LSTM
+                        x.append(feat_seq)
+                        y.append(target_seq)
+        return array(x), array(y)
+
+
+
+    class get_data(Dataset):
+        def __init__(self,feature,target):
+            self.feature = feature
+            self.target = target
+        def __len__(self):
+            return len(self.feature)
+        def __getitem__(self,idx):
+            item = self.feature[idx]
+            label = self.target[idx]
+            return item,label
+    
+    text, num_classes, int_to_vocab, vocab_to_int = open_data(data_file)#/home/scheppacha/data/trainset.txt')
+    
+    x,y = create_sequences(in_text = text, seq_size = seq_size, representation = representation, model_type = model_type)
+    
+    train_x, train_y = x[0:15000,:], y[0:15000]
+    
+    valid_x,valid_y = x[15000:20000,:], y[15000:20000]
+    
+    num_values_batch = batch_size*seq_size # "daten verbrauch" pro batch
+  
+    num_batches_train = train_y.shape[0] // (num_values_batch)# anzahl minibatches
+    end_train = num_values_batch*num_batches_train # macht keinen sinn, weil es bis 6.000.000 geht
+    
+    num_batches_valid = valid_y.shape[0] // (num_values_batch)# anzahl minibatches
+    end_valid = num_values_batch*num_batches_valid
+    
+    train_x, train_y = train_x[0:end_train,:], train_y[0:end_train]
+    valid_x, valid_y = valid_x[0:end_valid,:], valid_y[0:end_valid]
+    
+    train = get_data(train_x,train_y)# 
+    valid = get_data(valid_x, valid_y)
+    train_loader = torch.utils.data.DataLoader(train,batch_size,shuffle=True)#  
+    valid_loader = torch.utils.data.DataLoader(valid,batch_size,shuffle=True)
+    return train_loader, valid_loader, num_classes
+
---
+++ train_valid.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 09:12:06 2021
+
+@author: amadeu
+"""
+
+import csv
+import gc
+#import DeepVirFinder_model as dvf
+import data_preprocessing as dp
+import torch
+#from DeepVirFinder_model import model, device, criterion, optimizer, num_motifs, num_classes
+#from data_preprocessing import train_loader, valid_loader, batch_size, num_batches_valid, num_batches_train, valid_y
+
+import torch.nn as nn
+import numpy as np 
+from sklearn.metrics import classification_report
+
+
+
+train_losses = []
+valid_losses = []
+acc_train = []
+acc_test = []
+
+
+def Train(model, device, optimizer, criterion, train_loader, valid_loader, batch_size):
+    
+    running_loss = .0
+    
+    model.train() # dieses NN wurde oben als model definiert
+    
+    y_true_train = np.empty((0,1), int)
+    y_pred_train = np.empty((0,1), int)
+    
+    for idx, (inputs, labels) in enumerate(train_loader):
+        #print('inputs')
+        #print(inputs)
+        #print(inputs.shape) # 2,10,4
+        inputs = inputs.transpose(1, 2) # now 2,4,10
+        print('inputs_transposed')
+        print(inputs)
+        print(inputs.shape)
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        
+        inputs = inputs.type(torch.cuda.LongTensor)
+        print('inputs_long')
+        print(inputs)
+        print(inputs.shape) 
+        
+        optimizer.zero_grad()
+
+        logits = model(inputs)
+
+        labels = torch.max(labels, 1)[1]
+
+        #print('labels')
+        #print(labels)
+        #print(labels.shape)
+        loss = criterion(logits,labels.long())# label is of shape [batchsize], which means, we have a true integer class for each batch;
+        # preds is of shape [batchsize=32,num_classes=4], so we have probability for each batch, belonging to a certain class
+        #loss.requires_grad = True
+        logits = torch.max(logits, 1)[1]
+
+        labels, logits = labels.cpu().detach().numpy(), logits.cpu().detach().numpy()
+         
+        y_true_train = np.append(y_true_train, labels)
+        y_pred_train = np.append(y_pred_train, logits)
+        
+        loss_value = loss.item()
+
+        loss.backward()
+        optimizer.step()
+        running_loss += loss
+    
+    train_loss = running_loss/len(train_loader)
+    train_losses.append(train_loss.detach().cpu().numpy())
+    precRec = classification_report(y_pred_train, y_true_train, output_dict=True)
+    acc_train_epoch = precRec['accuracy']       
+    acc_train.append(acc_train_epoch)
+    
+    print(f'train_loss {train_loss}')
+    
+    np.save('train_loss', train_losses)
+    np.save('acc_train', acc_train)
+
+    np.save('preds_train', y_pred_train)
+    np.save('trues_train',y_true_train)
+    
+
+def Valid(model, device, optimizer, criterion, train_loader, valid_loader, batch_size):
+   
+    running_loss = .0
+    
+    model.eval()
+        
+    y_pred = np.zeros(1)
+    y_true = np.zeros(1)
+
+    with torch.no_grad():
+        for idx, (inputs, labels) in enumerate(valid_loader):
+            inputs = inputs.transpose(1, 2)
+            inputs = inputs.to(device) 
+            labels = labels.to(device)             
+            optimizer.zero_grad()
+            
+            logits = model(inputs.float())
+            labels = torch.max(labels, 1)[1]
+            loss = criterion(logits, labels.long())
+            running_loss += loss
+
+            logits = torch.max(logits, 1)[1]
+            labels, logits = labels.cpu().detach().numpy(), logits.cpu().detach().numpy()
+          
+            y_true = np.append(y_true, labels)
+            y_pred = np.append(y_pred, logits)
+            
+            
+        precRec = classification_report(y_pred, y_true, output_dict=True)
+        acc_test_epoch = precRec['accuracy']
+            
+        acc_test.append(acc_test_epoch)
+
+        valid_loss = running_loss/len(valid_loader)
+        valid_losses.append(valid_loss.detach().cpu().numpy())
+        print(f'valid_loss {valid_loss}')
+        
+    np.save('acc_test', acc_test)
+    np.save('valid_loss', valid_losses)
+    np.save('preds', y_pred)
+    np.save('trues',y_true)
+    #with open('preds.csv', 'w') as f: # logischerweise, macht er das für alle epochen, weshalb nur die letzte epoche abgespeichert wird (was uns nichts ausmacht, weil wir ja nur die prediction der letzten epoche haben wollen)
+    #    writer = csv.writer(f)
+    #    writer.writerow(y_pred)
+    #with open('trues.csv', 'w') as f: # logischerweise, macht er das für alle epochen, weshalb nur die letzte epoche abgespeichert wird (was uns nichts ausmacht, weil wir ja nur die prediction der letzten epoche haben wollen)
+    #    writer = csv.writer(f)
+    #    writer.writerow(y_true)
