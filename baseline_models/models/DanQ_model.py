a5ededaa290d366aac17e687a00631a36548200a
ascheppach <56842416+ascheppach@users.noreply.github.com>
2021-04-07 10:55:15 +0200

    Add files via upload

--- baseline_models/models/DanQ_model.py
+++ baseline_models/models/DanQ_model.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 19 14:06:32 2020
+
+@author: amadeu
+"""
+
 import csv
 
 import torch.nn as nn
@@ -11,6 +19,8 @@ import pandas as pd
 import numpy as np 
 import pandas as pd 
 import os
+import math
+
 
 from numpy import array
 import torch
@@ -22,13 +32,13 @@ from torch.utils.data import Dataset,DataLoader
 
 
 class NN_class(nn.Module):
-    def __init__(self,num_classes, num_motifs, batch_size):
+    def __init__(self,num_classes, num_motifs, batch_size, seq_size):
         super(NN_class,self).__init__()
         self.num_classes = num_classes
         self.num_motifs = num_motifs
         self.batch_size = batch_size
         self.conv1d = nn.Conv1d(num_classes,num_motifs,kernel_size=9, stride=1) # with seq_len= 1000 kernel_size=26; seq_len=150 kernelsize=9
-        # 
+        self.seq_size = seq_size
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool1d(kernel_size = 4, stride = 4) # seq_len=1000 kernel_size= 13 stride=13; seq_len=150 kernelsize=4 stride=4
         self.dropout = nn.Dropout(p=0.1)
@@ -36,7 +46,9 @@ class NN_class(nn.Module):
         self.lstm = nn.LSTM(num_motifs, #
                             num_motifs,
                             batch_first=True)
-        self.fc1 = nn.Linear(num_motifs*35,300) # seq_len 1000 75neuronen; seq_len 150 35neuronen
+        aux_num = (((seq_size-9)+1)-4)/4+1
+        self.num_neurons = math.floor(aux_num)
+        self.fc1 = nn.Linear(num_motifs*self.num_neurons,300) # seq_len 1000 75neuronen; seq_len 150 35neuronen
         self.fc2 = nn.Linear(300, num_classes)
        
         
@@ -57,7 +69,7 @@ class NN_class(nn.Module):
        
         x = self.dropout(x)
         
-        x = torch.reshape(output, (self.batch_size,35*self.num_motifs)) # seqlen=1000 75 neuronen; seq_len=150 35 neuronen
+        x = torch.reshape(output, (self.batch_size,self.num_neurons*self.num_motifs)) # seqlen=1000 75 neuronen; seq_len=150 35 neuronen
         #x = torch.flatten(x,1)
       
         x = self.fc1(x)
@@ -69,3 +81,25 @@ class NN_class(nn.Module):
     def zero_state(self, batch_size):
         return (torch.zeros(1, batch_size, self.num_motifs),
                torch.zeros(1, batch_size, self.num_motifs))
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#model = DanQ(num_classes, num_motifs, batch_size).to(device)
+#model = DanQ()
+
+criterion = nn.CrossEntropyLoss()
+#optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+
+
+#from modelsummary import summary
+#model = DanQ(num_classes=4,num_motifs=6)
+#x=torch.zeros((2,4,10))
+#state_h = torch.zeros((1,2,6))# für jeden b
+#state_c = torch.zeros((1,2,6))
+#prev_state = state_h, state_c
+#print(summary(model, x, prev_state, show_input=False)) # other output shapes than keras summary, as we receive lstm hiddenstate output instead of lstm output
+
+
+
+
