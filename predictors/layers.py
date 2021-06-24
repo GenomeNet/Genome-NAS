7c81b6627c0f42403e0a6db139316fd4071b963d
ascheppach <56842416+ascheppach@users.noreply.github.com>
2021-06-24 20:52:34 +0200

    Add files via upload

---
+++ predictors/layers.py
@@ -0,0 +1,48 @@
+import math
+
+import torch
+import torch.nn as nn
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+import torch.nn.functional as F
+
+# 1ter layer: in_features=nfeat, out_features=size
+# 2ter und alle restlichen layer: in_features=size, out_features=size
+
+
+# es werden im Endeffekt nur die Gewichte initialisiert mit einem shape/Dimension eben gemäß dem anzahl input_features (was von operations abhängt)
+# und out_features welche wir selber definieren
+class GraphConvolution(nn.Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input_, adj):
+        support = torch.matmul(input_, self.weight)
+        output = torch.bmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
