0c683f03d6ff4ff2dac69bfe6cd7e6f72beff6e7
ascheppach <56842416+ascheppach@users.noreply.github.com>
2021-07-13 12:41:16 +0200

    Add files via upload

--- baseline_models/models/DanQ_model.py
+++ baseline_models/models/DanQ_model.py
@@ -32,62 +32,105 @@ from torch.utils.data import Dataset,DataLoader
 
 
 class NN_class(nn.Module):
-    def __init__(self,num_classes, num_motifs, batch_size, seq_size):
+    def __init__(self, num_classes, num_motifs, batch_size, seq_size, task):
         super(NN_class,self).__init__()
         self.num_classes = num_classes
         self.num_motifs = num_motifs
         self.batch_size = batch_size
-        self.conv1d = nn.Conv1d(num_classes,num_motifs,kernel_size=9, stride=1) # with seq_len= 1000 kernel_size=26; seq_len=150 kernelsize=9
+        self.conv1d = nn.Conv1d(4,num_motifs,kernel_size=26, stride=1) # with seq_len= 1000 kernel_size=26; seq_len=150 kernelsize=9
         self.seq_size = seq_size
+        
         self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool1d(kernel_size = 4, stride = 4) # seq_len=1000 kernel_size= 13 stride=13; seq_len=150 kernelsize=4 stride=4
-        self.dropout = nn.Dropout(p=0.1)
+        self.maxpool = nn.MaxPool1d(kernel_size = 13, stride = 13) # seq_len=1000 kernel_size= 13 stride=13; seq_len=150 kernelsize=4 stride=4
+        self.dropout = nn.Dropout(p=0.2)
+        
+
         
         self.lstm = nn.LSTM(num_motifs, #
                             num_motifs,
-                            batch_first=True)
-        aux_num = (((seq_size-9)+1)-4)/4+1
-        self.num_neurons = math.floor(aux_num)
-        self.fc1 = nn.Linear(num_motifs*self.num_neurons,300) # seq_len 1000 75neuronen; seq_len 150 35neuronen
-        self.fc2 = nn.Linear(300, num_classes)
+                            #batch_first=True,
+                            bidirectional=True)
        
         
-    def forward(self,x,prev_state):
+        aux_num = (((seq_size-26)+1)-13)/13+1
+        self.dropout_2 = nn.Dropout(p=0.5)
+        self.num_neurons = math.floor(aux_num)
+        self.fc1 = nn.Linear(num_motifs*2*self.num_neurons,925) # seq_len 1000 75neuronen; seq_len 150 35neuronen
+        self.fc2 = nn.Linear(925, num_classes)
+        
+        if task == "TF_bindings":
+            
+            self.final = nn.Sigmoid()
+            
+        else:
+            
+            self.final = nn.Identity()
+            
+        # x = final(x)
+        # self.fc2 = nn.Linear(925, num_classes)
+
+    
+    def zero_state(self, batch_size):
+        return (torch.zeros(2, batch_size, self.num_motifs),
+               torch.zeros(2, batch_size, self.num_motifs))
+        
+    
+    def forward(self, x):
+        
       
         x = self.conv1d(x) 
         
         x = self.relu(x)
        
-        x = self.maxpool(x) 
-        x = torch.transpose(x,1, 2)
+        x = self.maxpool(x)
+        
+        x = self.dropout(x)
+        
+        h_0, c_0 = self.zero_state(self.batch_size)
+
+        
+        # x = torch.transpose(x, 1, 2)
+        x = x.permute(2,0,1)
+
+
         #CNN expects [batchsize, input_channels, signal_length]
         # lstm expects shape [batchsize, signal_length, number of features]
-     
-        x = self.dropout(x)
+        #print(x.shape)
+        #print(prev_state.shape)
+
+             
+        output, state = self.lstm(x, (h_0, c_0))
         
-        output, state = self.lstm(x, prev_state)
-       
-        x = self.dropout(x)
+        # x = torch.transpose(x, 1, 2)
+        x = output.permute(1,0,2)
         
-        x = torch.reshape(output, (self.batch_size,self.num_neurons*self.num_motifs)) # seqlen=1000 75 neuronen; seq_len=150 35 neuronen
+        x = torch.flatten(x, start_dim= 1) 
+
+
+        
+        #x = torch.reshape(output, (self.batch_size,self.num_neurons*self.num_motifs)) # seqlen=1000 75 neuronen; seq_len=150 35 neuronen
         #x = torch.flatten(x,1)
+        x = self.dropout_2(x)
+
       
         x = self.fc1(x)
       
         x = self.relu(x)
+        
         x = self.fc2(x)
-        return x, state
+        
+        x = self.final(x)
+        
+        return x#, state
     
-    def zero_state(self, batch_size):
-        return (torch.zeros(1, batch_size, self.num_motifs),
-               torch.zeros(1, batch_size, self.num_motifs))
+    
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #model = DanQ(num_classes, num_motifs, batch_size).to(device)
 #model = DanQ()
 
-criterion = nn.CrossEntropyLoss()
+#criterion = nn.CrossEntropyLoss()
 #optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 
 
---
+++ baseline_models/models/DeepSea.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 14:56:29 2021
+
+@author: amadeu
+"""
+
+
+import csv
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+from collections import Counter
+from argparse import Namespace
+import pandas as pd
+
+import numpy as np 
+import pandas as pd 
+import os
+
+from numpy import array
+import torch
+import gc
+from tqdm import tqdm_notebook as tqdm
+from torch.utils.data import Dataset,DataLoader
+#from pytorch_model_summary import summary
+import math
+
+
+
+   
+class NN_class(nn.Module):
+    def __init__(self, num_classes, batch_size, seq_size, task):
+        
+        super(NN_class,self).__init__()
+        
+        self.layer1 = nn.Sequential(
+                nn.Conv1d(4, 320, kernel_size=8),
+                nn.ReLU(inplace=True),
+                nn.MaxPool1d(kernel_size=4, stride=4),
+                nn.Dropout(p=0.2))
+        
+        self.layer2 = nn.Sequential(
+                nn.Conv1d(320, 480, kernel_size=8),
+                nn.ReLU(inplace=True),
+                nn.MaxPool1d(kernel_size=4, stride=4),
+                nn.Dropout(p=0.2))
+        
+        self.layer3 = nn.Sequential(
+                nn.Conv1d(480, 960, kernel_size=8),
+                nn.ReLU(inplace=True),
+                nn.Dropout(p=0.5))
+        
+        neur1 = math.floor((((seq_size-8)+1)-4)/4+1) # 248
+        neur2 = math.floor((((neur1-8)+1)-4)/4+1) # 59
+        neur3 = math.floor(((neur2-8)+1))
+
+        self.num_neurons = math.floor(neur3)
+        self.fc1 = nn.Linear(960*self.num_neurons, 925)   #  141/2 ; 4
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(925, num_classes)
+        
+        if task == "TF_bindings":
+            
+            self.final = nn.Sigmoid()
+            
+        else:
+            
+            self.final = nn.Identity()
+        
+      
+        
+    def forward(self, x):
+       
+        #print(x.shape)
+        x = self.layer1(x)# results in 91 neurons and 6 channels
+      
+        #print(x.shape)
+        x = self.layer2(x)
+       
+        x = self.layer3(x) #
+                
+        x = torch.flatten(x, start_dim= 1) 
+        
+        
+        x = self.fc1(x)
+        
+        x = self.relu(x)
+        
+        x = self.fc2(x)
+        x = self.final(x)
+        #print(x)
+        #print(x.shape)
+        
+        return x
