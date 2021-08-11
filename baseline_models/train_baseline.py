bd9e7041936dc3b37b58e8a67c663e0d7959be7e
ascheppach <56842416+ascheppach@users.noreply.github.com>
2021-08-11 20:14:14 +0200

    Add files via upload

--- baseline_models/train_baseline.py
+++ baseline_models/train_baseline.py
@@ -51,21 +51,23 @@ from generalNAS_tools.utils import scores_perClass, scores_Overall, pr_aucPerCla
 
 import matplotlib.pyplot as plt
 
+# from pytorchtools import EarlyStopping
 
 
-parser = argparse.ArgumentParser("train_LSTM_based")
+
+
+parser = argparse.ArgumentParser("train_baseline")
 parser.add_argument('--data', type=str, default='/home/amadeu/anaconda3/envs/darts_env/cnn/data2/trainset.txt', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=2, help='batch size')
+parser.add_argument('--batch_size', type=int, default=2, help='batch size') # 100
 parser.add_argument('--seq_size', type=int, default=1000, help='sequence size') # 200 oder 1000
-parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
-parser.add_argument('--epochs', type=int, default=10, help='num of training epochs')
+parser.add_argument('--learning_rate', type=float, default=0.001, help='init learning rate') # default RMSprob
+parser.add_argument('--epochs', type=int, default=10, help='num of training epochs') # min 60 but until convergence
 parser.add_argument('--test_epochs', type=int, default=1, help='num of testing epochs')
 
 # parser.add_argument('--num_classes', type=int, default=4, help='num of target classes')
-parser.add_argument('--task', type=str, default='next_character_prediction', help='defines the task')#TF_bindings
-parser.add_argument('--num_steps', type=int, default=7, help='number of iterations per epoch')
-parser.add_argument('--test_num_steps', type=int, default=7, help='number of iterations per testing epoch')
-
+parser.add_argument('--task', type=str, default='TF_bindings', help='defines the task')#TF_bindings
+parser.add_argument('--num_steps', type=int, default=2, help='number of iterations per epoch')
+parser.add_argument('--test_num_steps', type=int, default=2, help='number of iterations per testing epoch')
 
 parser.add_argument('--valid_directory', type=str, default='/home/amadeu/Downloads/genomicData/validation', help='directory of validation data')
 parser.add_argument('--train_directory', type=str, default='/home/amadeu/Downloads/genomicData/train', help='directory of training data')
@@ -82,15 +84,17 @@ parser.add_argument('--valid_target_directory', type=str, default='/home/amadeu/
 parser.add_argument('--test_input_directory', type=str, default='/home/amadeu/Desktop/GenomNet_MA/data/inputs_small_test.pkl', help='directory of test input data')
 parser.add_argument('--test_target_directory', type=str, default='/home/amadeu/Desktop/GenomNet_MA/data/targets_small_test.pkl', help='directory of test target data')
 
-parser.add_argument('--num_motifs', type=int, default=100, help='number of channels') # 320
+# parser.add_argument('--num_motifs', type=int, default=100, help='number of channels') # 320
 parser.add_argument('--model', type=str, default='DanQ', help='path to save the model')
-parser.add_argument('--save', type=str,  default='EXP',
+parser.add_argument('--save', type=str,  default='bas',
                     help='path to save the labels and predicitons')
 parser.add_argument('--model_path', type=str,  default='./danQ_net.pth',
                     help='path to save the trained model')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-parser.add_argument('--report_freq', type=int, default=5, help='validation report frequency')
+parser.add_argument('--report_validation', type=int, default=1, help='validation report frequency')
+parser.add_argument('--improv', type=float, default=0, help='minimum change to define an improvement')
+parser.add_argument('--patience', type=int, default=5, help='how many epochs with no improvement after which training will be stopped')
 args = parser.parse_args()
 
 
@@ -113,6 +117,7 @@ def main():
   
   # criterion = nn.BCELoss()
   
+  
   if (args.task == "next_character_prediction"):
       import generalNAS_tools.data_preprocessing_new as dp
       
@@ -121,20 +126,22 @@ def main():
       
       _, test_queue, _ = dp.data_preprocessing(train_directory = args.train_directory, valid_directory = args.test_directory, num_files=args.num_files,
                 seq_size = args.seq_size, batch_size=args.batch_size, next_character=args.next_character_prediction)
-     
+      criterion = nn.CrossEntropyLoss().to(device)
+      num_classes = 4
+
 
       if (args.model == "DanQ"):
           import baseline_models.models.DanQ_model as model
 
-          model = model.NN_class(num_classes, args.num_motifs, args.batch_size, args.seq_size, args.task).to(device)
+          model = model.NN_class(num_classes, args.batch_size, args.seq_size, args.task).to(device)
           
-          criterion = nn.CrossEntropyLoss().to(device)
           optimizer = torch.optim.RMSprop(model.parameters(), lr=args.learning_rate)
           
       if (args.model == "DeepSEA"):
+          
           import baseline_models.models.DeepSea as model
           model = model.NN_class(num_classes, args.batch_size, args.seq_size, args.task).to(device)
-          criterion = nn.CrossEntropyLoss().to(device)
+
           optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=1e-6, momentum=0.9)
         
       
@@ -143,16 +150,17 @@ def main():
       
       import generalNAS_tools.data_preprocessing_TF as dp
         
-      train_queue, valid_queue, test_queue = dp.data_preprocessing(args.train_input_directory, args.valid_input_directory, args.test_input_directory, args.train_target_directory, args.valid_target_directory, args.test_target_directory, args.batch_size)
-      
+      # train_queue, valid_queue, test_queue = dp.data_preprocessing(args.train_input_directory, args.valid_input_directory, args.test_input_directory, args.train_target_directory, args.valid_target_directory, args.test_target_directory, args.batch_size)
+      train_queue, valid_queue, test_queue = dp.data_preprocessing(args.train_directory, args.valid_directory, args.test_directory, args.batch_size)
+
       num_classes = 919
       
+      criterion = nn.BCELoss().to(device)
+
       if (args.model == "DanQ"):
           
           import baseline_models.models.DanQ_model as model
-
-          model = model.NN_class(num_classes, args.num_motifs, args.batch_size, args.seq_size, args.task).to(device)
-          criterion = nn.BCELoss().to(device)
+          model = model.NN_class(num_classes, args.batch_size, args.seq_size, args.task).to(device)
           optimizer = torch.optim.RMSprop(model.parameters(), lr=args.learning_rate)
 
           
@@ -160,8 +168,7 @@ def main():
           
           import baseline_models.models.DeepSea as model
 
-          model = models.DanQ_model.NN_class(num_classes, args.num_motifs, args.batch_size, args.seq_size, args.task).to(device)
-          criterion = nn.BCELoss().to(device)
+          model = model.NN_class(num_classes, args.batch_size, args.seq_size, args.task).to(device)
           optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=1e-6, momentum=0.9)
 
   
@@ -177,15 +184,16 @@ def main():
   valid_acc = []
   #all_predictions_valid = []
   time_per_epoch = []
-
+  cnt=0
 
   for epoch in range(args.epochs):
       # epoch=0
       
       train_start = time.strftime("%Y%m%d-%H%M")
+      epoch_start = time.time()
 
       #train_loss, acc_train_epoch = Train(model, train_queue, optimizer, criterion, device, args.num_steps, args.report_freq)
-      labels, predictions, train_loss = Train(model, train_queue, optimizer, criterion, device, args.num_steps, args.report_freq)
+      labels, predictions, train_loss = Train(model, train_queue, optimizer, criterion, device, args.num_steps)
 
       labels = np.concatenate(labels)
       predictions = np.concatenate(predictions)
@@ -199,12 +207,9 @@ def main():
 
       else:
           f1 = overall_f1(labels, predictions, args.task)
-
           logging.info('| epoch {:3d} | train f1-score {:5.2f}'.format(epoch, f1))
           train_acc.append(f1)
-
-
-      
+          
       # np.argmax(predictions, axis=1)
       # np.round(predictions)
 
@@ -213,80 +218,131 @@ def main():
       train_losses.append(train_loss)
       epoch_end = time.time()
       time_per_epoch.append(epoch_end)
+      epoch_end = time.time()
+      epoch_duration = epoch_end - epoch_start
+      logging.info('Epoch time: %ds', epoch_duration)
 
       # acc_train.append(acc_train_epoch)
       # np.save('train_loss', train_losses)
       # np.save('acc_train', acc_train)
       
-      if epoch % args.report_freq == 0:
+      if epoch % args.report_validation == 0:
           
-          labels, predictions, valid_loss = Valid(model, valid_queue, optimizer, criterion, device, args.num_steps, args.report_freq)
+          labels, predictions, valid_loss = Valid(model, valid_queue, optimizer, criterion, device, args.num_steps)
           
           labels = np.concatenate(labels)
           predictions = np.concatenate(predictions)
           
           if args.task == ('next_character_prediction'):
               acc = overall_acc(labels, predictions, args.task)
-          
-              logging.info('| epoch {:3d} | train acc {:5.2f}'.format(epoch, acc))
-              valid_acc.append(acc)
+              logging.info('| epoch {:3d} | val acc {:5.2f}'.format(epoch, acc))
+              logging.info('| epoch {:3d} | val loss {:5.2f}'.format(epoch, valid_loss))
 
+              valid_acc.append(acc)
 
           else:
               f1 = overall_f1(labels, predictions, args.task)
+              logging.info('| epoch {:3d} | val f1-score {:5.2f}'.format(epoch, f1))
+              logging.info('| epoch {:3d} | val loss {:5.2f}'.format(epoch, valid_loss))
 
-              logging.info('| epoch {:3d} | train f1-score {:5.2f}'.format(epoch, f1))
               valid_acc.append(f1)
+           
+          # Early stopping
+          if epoch > 0:
+              if (valid_loss+args.improv) < valid_losses[epoch-1]:
+                  cnt = 0
+              else:
+                  cnt += 1
+                  
+              if cnt == args.patience:
+                  
+                  valid_losses.append(valid_loss)
+                  torch.save(model, args.model_path)
+  
+                  trainloss_file = '{}-train_loss-{}'.format(args.save, train_start)
+                  np.save(trainloss_file, train_losses)
+                  acc_train_file = '{}-acc_train-{}'.format(args.save, train_start)
+                  np.save(acc_train_file, train_acc)
+                  # predictions__train_file = '{}-predictions_train-{}'.format(args.save, train_start)
+                  # np.save(predictions__train_file, all_predictions_train)
+  
+                  time_file = '{}-time-{}'.format(args.save, train_start)
+                  np.save(time_file, time_per_epoch)
+      
 
-          
+                  # safe valid data
+                  validloss_file = '{}-valid_loss-{}'.format(args.save, train_start)
+                  np.save(validloss_file, valid_losses)
+                  acc_valid_file = '{}-acc_valid-{}'.format(args.save, train_start)
+                  np.save(acc_valid_file, valid_acc)
+                  break
+                    
+                  
+              
           valid_losses.append(valid_loss)
-          # all_labels_valid.append(labels)
-          # acc_valid.append(acc_test_epoch)
-          # np.save('acc_test', acc_test)
-          # np.save('valid_loss', valid_losses)
-         
         
-      # safe train data
-  #PATH = './danQ_net.pth'
-  torch.save(model, args.model_path)
-  
-  trainloss_file = '{}-train_loss-{}'.format(args.save, train_start)
-  np.save(trainloss_file, train_losses)
-  acc_train_file = '{}-acc_train-{}'.format(args.save, train_start)
-  np.save(acc_train_file, train_acc)
-  # predictions__train_file = '{}-predictions_train-{}'.format(args.save, train_start)
-  # np.save(predictions__train_file, all_predictions_train)
-  
-  time_file = '{}-time-{}'.format(args.save, train_start)
-  np.save(time_file, time_per_epoch)
-      
-
-  # safe valid data
-  validloss_file = '{}-valid_loss-{}'.format(args.save, train_start)
-  np.save(validloss_file, valid_losses)
-  acc_valid_file = '{}-acc_valid-{}'.format(args.save, train_start)
-  np.save(acc_valid_file, valid_acc)
-  # predictions__valid_file = '{}-predictions_valid-{}'.format(args.save, train_start)
-  # np.save(predictions__valid_file, all_predictions_valid)
+         
+     
       
   
-  # test 
+  #### test ### 
   test_losses = []
   all_labels_test = []
   all_predictions_test = []
 
   train_start = time.strftime("%Y%m%d-%H%M")
   
-  for epoch in range(args.test_epochs): # 1 epoch has 3000 iterations time 32 batchsize is 96000 samples
-      
-      labels, predictions, test_loss = Valid(model, test_queue, optimizer, criterion, device, args.test_num_steps, args.report_freq)
+  #for epoch in range(args.test_epochs): # 1 epoch has 3000 iterations time 32 batchsize is 96000 samples
+  
+  objs = utils.AvgrageMeter()
+  #top1 = utils.AvgrageMeter()
+  #top5 = utils.AvgrageMeter()
+    
+  total_loss = 0
+  start_time = time.time()
+    
+  model.eval()
+
+  labels = []
+  predictions = []
+  scores = nn.Softmax()
+
+
+  with torch.no_grad():
+        
+      for idx, (inputs, targets) in enumerate(test_queue):
+        
+          input, label = inputs, targets
+               
+          input = input.float().to(device)#.cuda()
+            
+          #label = torch.max(label, 1)[1]
+          label = label.to(device)#.cuda(non_blocking=True)
+          batch_size = input.size(0)
+        
+          #if args.task == "TF_bindings":
+          logits = model(input.float()) #, (state_h, state_c))
+
+          loss = criterion(logits, label)
+          #else:
+              # label = torch.max(label, 1)[1]
+          #logits = model(input.float()) #, (state_h, state_c))
+            
+          labels.append(label.detach().numpy())
+          if args.task == "next_character_prediction":
+              predictions.append(scores(logits).detach().numpy())
+          else:
+              predictions.append(logits.detach().numpy())
+                
+          objs.update(loss.data, batch_size)
+    
           
-      labels = np.concatenate(labels)
-      predictions = np.concatenate(predictions)
+  labels = np.concatenate(labels)
+  predictions = np.concatenate(predictions)
       
-      test_losses.append(test_loss)
-      all_labels_test.append(labels)
-      all_predictions_test.append(predictions)
+  test_losses.append(test_loss)
+  all_labels_test.append(labels)
+  all_predictions_test.append(predictions)
       
 
   testloss_file = '{}-test_loss-{}'.format(args.save, train_start)
@@ -302,7 +358,7 @@ def main():
 
 # train_loader, num_steps = train_queue, 7
 
-def Train(model, train_loader, optimizer, criterion, device, num_steps, report_freq):
+def Train(model, train_loader, optimizer, criterion, device, num_steps):
         
     objs = utils.AvgrageMeter()
  
@@ -333,7 +389,7 @@ def Train(model, train_loader, optimizer, criterion, device, num_steps, report_f
 
         loss = criterion(logits, label)#.long()) 
      
-        labels.append(label)
+        labels.append(label.detach().numpy())
         if args.task == "next_character_prediction":
             predictions.append(scores(logits).detach().numpy())
         else:#if args.task == "TF_bindings"::
@@ -347,7 +403,7 @@ def Train(model, train_loader, optimizer, criterion, device, num_steps, report_f
     return labels, predictions, objs.avg
     
 
-def Valid(model, valid_loader, optimizer, criterion, device, num_steps, report_freq):
+def Valid(model, valid_loader, optimizer, criterion, device, num_steps):
    
     objs = utils.AvgrageMeter()
     #top1 = utils.AvgrageMeter()
@@ -388,11 +444,12 @@ def Valid(model, valid_loader, optimizer, criterion, device, num_steps, report_f
                 # label = torch.max(label, 1)[1]
             #logits = model(input.float()) #, (state_h, state_c))
             
-            labels.append(label)
+            labels.append(label.detach().numpy())
             if args.task == "next_character_prediction":
                 predictions.append(scores(logits).detach().numpy())
             else:
                 predictions.append(logits.detach().numpy())
+                
             objs.update(loss.data, batch_size)
   
     return labels, predictions, objs.avg #top1.avg, objs.avg
