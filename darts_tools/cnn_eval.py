b05f88ade14dc207edd78ec91c71cf3b9a7f68b4
ascheppach <56842416+ascheppach@users.noreply.github.com>
2021-08-12 19:37:15 +0200

    Add files via upload

--- darts_tools/cnn_eval.py
+++ darts_tools/cnn_eval.py
@@ -15,30 +15,28 @@ from torch.autograd import Variable
 from generalNAS_tools.utils import drop_path
 
 
-# genotype, C_prev_prev, C_prev, C_curr, reduction, reduction_prev
-
-
+# genotype, C_prev_prev, C_prev, C_curr, reduction, reduction_prev, reduction_high
 class CNN_Cell_eval(nn.Module):
 
-  def __init__(self, genotype, C_prev_prev, C_prev, C, reduction, reduction_prev):
+  def __init__(self, genotype, C_prev_prev, C_prev, C, reduction, reduction_prev, reduction_high):
     super(CNN_Cell_eval, self).__init__()
     self.reduction = reduction
 
     if reduction_prev:
-      self.preprocess0 = FactorizedReduce(C_prev_prev, C)
+      self.preprocess0 = FactorizedReduce(C_prev_prev, C, 2)
     else:
       self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0)
+      
     self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0)
     
     # receive the operations from genotype
     if reduction:
       ## meine Version ##
-      geno_reduce = genotype[2]
-      # geno_reduce = hb_results[0]
-      op_names = []
+      geno_reduce = genotype[2] # operations and edges from reduction cells
+      op_names = [] # operations from reduction cells
       for op_name in geno_reduce:
           op_names.append(op_name[0])
-      indices = []
+      indices = [] # edges from reduction cells
       for idx in geno_reduce:
           indices.append(idx[1])
       concat = genotype[3]
@@ -56,10 +54,10 @@ class CNN_Cell_eval(nn.Module):
       concat = genotype[1]
       
     # C, reduction = 8, False
-    self._compile(C, op_names, indices, concat, reduction)
+    self._compile(C, op_names, indices, concat, reduction, reduction_high)
 
   # in order to receive "_ops" object, with all 8 operations 
-  def _compile(self, C, op_names, indices, concat, reduction):
+  def _compile(self, C, op_names, indices, concat, reduction, reduction_high):
     assert len(op_names) == len(indices)
     self._steps = len(op_names) // 2
     self._concat = concat
@@ -67,7 +65,16 @@ class CNN_Cell_eval(nn.Module):
 
     self._ops = nn.ModuleList()
     for name, index in zip(op_names, indices):
-      stride = 2 if reduction and index < 2 else 1
+      # stride = 2 if reduction and index < 2 else 1
+      if (reduction==False) or index >= 2:
+         stride=1
+                
+      if reduction and index < 2:
+         stride=2
+                    
+      if reduction_high and index < 2:
+         stride=3
+         
       op = OPS[name](C, stride, True)
       self._ops += [op]
     self._indices = indices
@@ -80,7 +87,7 @@ class CNN_Cell_eval(nn.Module):
 
     states = [s0, s1]
     for i in range(self._steps):
-      h1 = states[self._indices[2*i]] # because for 1th Node only need 0th element and 
+      h1 = states[self._indices[2*i]] # because for 1th Node only need 0th element of states; for 2th Node need 2th element; 3th Node 4the element
       h2 = states[self._indices[2*i+1]] # 1th element of states (for 2th Node we need 2th and 3th element of states)
       op1 = self._ops[2*i]
       op2 = self._ops[2*i+1]
@@ -94,3 +101,10 @@ class CNN_Cell_eval(nn.Module):
       s = h1 + h2
       states += [s]
     return torch.cat([states[i] for i in self._concat], dim=1)
+
+
+
+
+
+
+
